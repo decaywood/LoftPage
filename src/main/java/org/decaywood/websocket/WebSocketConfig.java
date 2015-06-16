84d29ea5b061d2486447837107fde1c03bd267d6
decaywood <decaywood@qq.com>
2015-06-16 21:57:43 +0800

    add websocket

---
+++ src/main/java/org/decaywood/websocket/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package org.decaywood.websocket;
+
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * @author: decaywood
+ * @date: 2015/6/16 21:51
+ */
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+
+    }
+
+}
