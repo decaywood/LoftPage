95be75f657a916d84ed4593ccadb7f08fea304ef
decaywood <decaywood@qq.com>
2015-06-18 23:49:10 +0800

    build buffer

--- src/main/java/org/decaywood/buffer/MessageBuffer.java
+++ src/main/java/org/decaywood/buffer/MessageBuffer.java
@@ -2,6 +2,7 @@ package org.decaywood.buffer;
 
 import org.decaywood.entity.KeyEvent;
 
+import java.util.Deque;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
@@ -13,38 +14,51 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class MessageBuffer {
 
-    private ConcurrentHashMap<Integer, ConcurrentLinkedDeque<KeyEvent>> pool;
+    private ConcurrentHashMap<Integer, Deque<KeyEvent>> bufferPool;
 
-    private int maxSizePerDeque;
+    private int threshold;
 
     public MessageBuffer() {
         this(1 << 8);
     }
 
-    public MessageBuffer(int maxSizePerDeque) {
+    public MessageBuffer(int threshold) {
 
-        this.maxSizePerDeque = maxSizePerDeque > 0 ? maxSizePerDeque : 1 << 8;
+        this.threshold = threshold > 0 ? threshold : 1 << 8;
 
-        this.pool = new ConcurrentHashMap<>(4);
-        for (int i = 0; i < pool.size(); i++) {
-            this.pool.put(i, new ConcurrentLinkedDeque<>());
+        this.bufferPool = new ConcurrentHashMap<>(4);
+        for (int i = 0; i < bufferPool.size(); i++) {
+            this.bufferPool.put(i, new ConcurrentLinkedDeque<>());
         }
     }
 
     public void publishEvent(KeyEvent keyEvent) {
 
-        long id = keyEvent.getID();
-
+        Deque<KeyEvent> buffer = getMinBuffer();
+        buffer.offer(keyEvent);
 
     }
+
+    private synchronized Deque<KeyEvent> getMinBuffer() {
+        if(averageSize() > this.threshold){
+            this.bufferPool.put(this.bufferPool.size(), new ConcurrentLinkedDeque<>());
+        }
+        Deque<KeyEvent> minSizeBuffer = bufferPool.reduceValues(
+                Integer.MAX_VALUE,
+                (first, second) -> first.size() < second.size() ? first : second);
+        return minSizeBuffer;
+    }
+    
 
     private int averageSize() {
-        int sizeSum = pool.reduceToInt(
+        int sizeSum = bufferPool.reduceToInt(
                 Integer.MAX_VALUE,
-                (first, second) -> second.size(),
+                (key, value) -> value.size(),
                 0,
                 (first, second) -> first + second);
-        return sizeSum / pool.size();
+        return sizeSum / bufferPool.size();
     }
+
+   
 
 }
